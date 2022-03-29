715c29a79461dfa8adc5cb1ee0c52c99f0c2c6db
Dorin Dorsman <dorsmandorin@gmail.com>
2022-03-29 14:35:28 +0300

    bottom app bar

--- app/src/main/java/com/dorin/smartravel/MainActivity.java
+++ app/src/main/java/com/dorin/smartravel/MainActivity.java
@@ -3,12 +3,30 @@ package com.dorin.smartravel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViews();
+
+
+    }
+
+    private void findViews() {
+
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
     }
 }
